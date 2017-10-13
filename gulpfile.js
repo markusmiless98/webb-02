047aa9ede7aefde6cd10ffbf957a0f50ebe4b163
markusmiless98 <markusmiless98@users.noreply.github.com>
2017-10-13 08:32:02 +0200

    Uppgifter 1-6

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,21 @@
-var gulp = require('gulp');
-var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
- 
-var paths = {
-  scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
-  images: 'client/img/**/*'
-};
- 
-// Not all tasks need to use streams 
-// A gulpfile is just another node program and you can use any package available on npm 
-gulp.task('clean', function() {
-  // You can use multiple globbing patterns as you would with `gulp.src` 
-  return del(['build']);
-});
- 
-gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts) 
-  // with sourcemaps all the way down 
-  return gulp.src(paths.scripts)
-    .pipe(sourcemaps.init())
-      .pipe(coffee())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/js'));
-});
- 
-// Copy all static images 
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task 
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-});
- 
-// Rerun the task when a file changes 
-gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
-});
- 
-// The default task (called when you run `gulp` from cli) 
-gulp.task('default', ['watch', 'scripts', 'images']);
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+// Standardkörning
+gulp.task('default', ['copyhtml', 'sass'])
+// Gulp Watch
+gulp.task('watch', function(){
+  gulp.watch('src/*.hmtl', ['copyhtml']);
+  gulp.watch('src/sass/*.scss', ['sass']);
+})
+
+gulp.task('copyhtml', function(){
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('build'));
+})
+// Uppdaterar css filer
+gulp.task('sass', function(){
+  gulp.src('src/sass/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('build/css'))
+})
